79ccc2fcabd8026f91a9188dfd2d9065d3cddd1f
neco3coffee <neco3coffee@gmail.com>
2022-09-17 23:21:35 +0900

    [add] Footerの追加,BaseLayoutにFooter追加 #4

--- src/components/Templates/BaseLayout.tsx
+++ src/components/Templates/BaseLayout.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Header } from '@/components/Organisms/Header'
+import { Footer } from '@/components/Organisms/Footer'
 
 export const BaseLayout = (props) => {
   const { children } = props
@@ -7,6 +8,7 @@ export const BaseLayout = (props) => {
     <>
       <Header />
       {children}
+      <Footer />
     </>
   )
 }
