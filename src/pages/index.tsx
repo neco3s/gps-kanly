39208e2713974901309e53ebdb97bc8338aaaee1
neco3coffee <neco3coffee@gmail.com>
2022-09-17 23:24:01 +0900

    [add] ホームページにHeroとFeatureSectionを追加 #4

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,17 @@
 import { Box, Flex, Spacer, Stack, VStack, HStack, Text, Heading, Button } from '@chakra-ui/react'
 import React from 'react'
 import { BaseLayout } from '@/components/Templates/BaseLayout'
+import { Hero } from '@/components/Molecules/Hero'
+import { FeatureCard } from '@/components/Molecules/FeatureCard'
+import { FeatureSection } from '@/components/Organisms/FeatureSection'
 
 export default function Home() {
   return (
     <>
-      <BaseLayout>home</BaseLayout>
+      <BaseLayout>
+        <Hero />
+        <FeatureSection />
+      </BaseLayout>
     </>
   )
 }
