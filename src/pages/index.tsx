35a2ef83543be3b9a0def276cf6c47ad244c080f
neco3coffee <neco3coffee@gmail.com>
2022-09-17 16:19:33 +0900

    [fix] import文の順序を変更、eslintの設定を変更

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import React from 'react'
 import { Box, Flex, Spacer, Stack, VStack, HStack, Text, Heading, Button } from '@chakra-ui/react'
+import React from 'react'
 import { BaseLayout } from '@/components/Templates/BaseLayout'
 
 export default function Home() {
