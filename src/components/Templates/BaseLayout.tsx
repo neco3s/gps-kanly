7d491c93e3f567a0814c59bf54c255c6cfe3a536
neco3coffee <neco3coffee@gmail.com>
2022-09-18 11:57:23 +0900

    [fix] footerの下に余白が生まれる問題の修正

--- src/components/Templates/BaseLayout.tsx
+++ src/components/Templates/BaseLayout.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { Header } from '@/components/Organisms/Header'
 import { Footer } from '@/components/Organisms/Footer'
+import { Box } from '@chakra-ui/react'
 
 export const BaseLayout = (props) => {
   const { children } = props
   return (
     <>
-      <Header />
-      {children}
-      <Footer />
+      <Box position='relative' minH='100vh'>
+        <Header />
+        {children}
+        <Footer />
+      </Box>
     </>
   )
 }
